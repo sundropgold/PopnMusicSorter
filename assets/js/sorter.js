61df6d906f757b2c364054a14801d2eabc992e9e
Flynn <flynnigan.tan@gmail.com>
2017-06-24 10:04:44 -0500

    updated html and css files

---
+++ assets/js/sorter.js
@@ -0,0 +1,29 @@
+$(document).on(ready, function(){
+
+	// array of characters unsorted
+	var popnUnsorted = [];
+	var popnUnsortedImg = [];
+
+	// array of characters sorted
+	var popnSorted = [];
+	var popnSortedImg = [];
+
+	// while characters are not yet sorted, continue looping
+
+	// keep track of battle numbers
+
+	// how much percent sorted
+
+	// picture/button one = +1 point
+
+	// picture/button two = +1 point
+
+	// love them both = +1 point to both
+
+	// hate them both = -1 point to both
+
+	// neutral = nothing happens
+
+	// table of results
+	// rank - character img/name - number of points
+});
